917452b16843a26a8b085ddacd9d7fe50fc50cc8
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-05-20 16:25:31 +0530

    Mark private members private

--- src/modules/temperature_display.js
+++ src/modules/temperature_display.js
@@ -2,42 +2,42 @@ export { UITemperatureDisplay };
 
 class UITemperatureDisplay {
   element;
-  temperatureElement;
-  unitsElement;
-  celsiusButton;
-  fahrenheitButton;
+  #temperatureElement;
+  #unitsElement;
+  #celsiusButton;
+  #fahrenheitButton;
 
-  activeUnit = "";
-  temperatureData = {}; // {c: , f: }
+  #activeUnit = "";
+  #temperatureData = {}; // {c: , f: }
 
   constructor() {
     //Create Elements
     this.element = document.createElement("div");
-    this.temperatureElement = document.createElement("span");
+    this.#temperatureElement = document.createElement("span");
 
-    this.unitsElement = document.createElement("span");
-    this.celsiusButton = document.createElement("button");
-    this.fahrenheitButton = document.createElement("button");
+    this.#unitsElement = document.createElement("span");
+    this.#celsiusButton = document.createElement("button");
+    this.#fahrenheitButton = document.createElement("button");
 
     //Set Attributes
     this.element.className = "temperature";
 
-    this.temperatureElement.className = "value";
+    this.#temperatureElement.className = "value";
 
-    this.unitsElement.className = "units";
-    this.celsiusButton.className = "celsius";
-    this.celsiusButton.textContent = "°C";
+    this.#unitsElement.className = "units";
+    this.#celsiusButton.className = "celsius";
+    this.#celsiusButton.textContent = "°C";
     this.toggleUnit();
 
-    this.fahrenheitButton.className = "fahrenheit";
-    this.fahrenheitButton.textContent = "°F";
+    this.#fahrenheitButton.className = "fahrenheit";
+    this.#fahrenheitButton.textContent = "°F";
 
     //Append Elements
-    this.unitsElement.append(this.celsiusButton, this.fahrenheitButton);
-    this.element.append(this.temperatureElement, this.unitsElement);
+    this.#unitsElement.append(this.#celsiusButton, this.#fahrenheitButton);
+    this.element.append(this.#temperatureElement, this.#unitsElement);
 
     //Add listeners
-    this.unitsElement.addEventListener("click", (e) => {
+    this.#unitsElement.addEventListener("click", (e) => {
       if (e.target.tagName === "BUTTON") {
         if (e.target.classList.contains("celsius")) {
           this.setCelsiusActive();
@@ -49,39 +49,39 @@ class UITemperatureDisplay {
   }
 
   update(temperature) {
-    this.temperatureData = temperature;
+    this.#temperatureData = temperature;
     this.updateDisplay();
   }
 
   updateDisplay() {
-    if (this.temperatureData[this.activeUnit]) {
-      this.temperatureElement.textContent =
-        this.temperatureData[this.activeUnit];
+    if (this.#temperatureData[this.#activeUnit]) {
+      this.#temperatureElement.textContent =
+        this.#temperatureData[this.#activeUnit];
     } else {
-      this.temperatureElement.textContent = "0";
+      this.#temperatureElement.textContent = "0";
     }
   }
 
   setCelsiusActive() {
-    this.activeUnit = "c";
+    this.#activeUnit = "c";
 
-    this.celsiusButton.classList.add("active");
-    this.fahrenheitButton.classList.remove("active");
+    this.#celsiusButton.classList.add("active");
+    this.#fahrenheitButton.classList.remove("active");
 
     this.updateDisplay();
   }
 
   setFahrenheitActive() {
-    this.activeUnit = "f";
+    this.#activeUnit = "f";
 
-    this.fahrenheitButton.classList.add("active");
-    this.celsiusButton.classList.remove("active");
+    this.#fahrenheitButton.classList.add("active");
+    this.#celsiusButton.classList.remove("active");
 
     this.updateDisplay();
   }
 
   toggleUnit() {
-    if (this.activeUnit === "c") {
+    if (this.#activeUnit === "c") {
       this.setFahrenheitActive();
     } else {
       this.setCelsiusActive();
