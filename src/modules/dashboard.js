8bac245564445ebb911bab703a528e32e544124d
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-05-21 14:50:49 +0530

    Add dashboard class

---
+++ src/modules/dashboard.js
@@ -0,0 +1,61 @@
+import { UIWeatherIcon } from "./weather_icon";
+import { UISearchComponent } from "./search_component";
+import { UITemperatureDisplay } from "./temperature_display";
+import { UIInformationDisplay } from "./information_display";
+
+export { UIDashboard };
+
+class UIDashboard {
+  element;
+  weatherIcon;
+  searchComponent;
+  temperatureDisplay;
+  informationDisplay;
+
+  currentData;
+
+  constructor() {
+    //Create Elements
+    this.element = document.createElement("div");
+    this.weatherIcon = new UIWeatherIcon();
+    this.searchComponent = new UISearchComponent();
+    this.temperatureDisplay = new UITemperatureDisplay();
+    this.informationDisplay = new UIInformationDisplay();
+
+    //Set Attributes
+    this.element.className = "dashboard";
+
+    //Append Elements
+    this.element.append(
+      this.weatherIcon.element,
+      this.searchComponent.element,
+      this.temperatureDisplay.element,
+      this.informationDisplay.element,
+    );
+  }
+
+  updateIcon(url) {
+    this.weatherIcon.setURL(url);
+  }
+
+  updateSearch(results) {
+    this.searchComponent.updateResults(results);
+  }
+
+  updateLocation(location) {
+    this.searchComponent.lockLocation(location);
+  }
+
+  updateTemperature(temperature) {
+    this.temperatureDisplay.update(temperature);
+  }
+
+  updateMiscData(data) {
+    this.informationDisplay.update(data);
+  }
+
+  setUnits(units) {
+    this.temperatureDisplay.setActiveUnits(units);
+    this.informationDisplay.setActiveUnits(units);
+  }
+}
